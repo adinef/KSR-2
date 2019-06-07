39b8ef839e360db6d92d1a4214698ba854a915c9
s207230 <207230@edu.p.lodz.pl>
2019-06-07 16:52:42 +0200

    Test example for summarization

--- src/main/java/net/script/TestInitMain.java
+++ src/main/java/net/script/TestInitMain.java
@@ -4,18 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import net.script.data.csv.CsvReader;
 import net.script.data.entities.DCResMeasurement;
 import net.script.logic.fuzzy.FuzzySet;
+import net.script.logic.fuzzy.functions.RectangularFunction;
 import net.script.logic.fuzzy.functions.TriangleFunction;
 import net.script.logic.fuzzy.linguistic.Range;
 import net.script.logic.qualifier.Qualifier;
-import org.slf4j.LoggerFactory;
+import net.script.logic.quantifier.Quantifier;
+import net.script.logic.summarizer.Summarizer;
+import net.script.view.Summary;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.LogManager;
 
 @Configuration
@@ -38,16 +41,72 @@ public class TestInitMain {
         System.setOut(original);
         System.out.println("Finished data load up.");
         log.info("Fuzzy set creation start");
-        FuzzySet fz = FuzzySet.with(dcResMeasurements).from(new Qualifier("many bathrooms","BATHRM", new TriangleFunction(5D,2D),new Range(3D,4D)));
-        assert FuzzySet.intersect(fz, fz).size() == fz.size();
+
+
+        Qualifier bathrooms = new Qualifier("dużo łazienek", "BATHRM", new TriangleFunction(5D, 2D), new Range(3D, 4D));
+        Qualifier landarea = new Qualifier("ogromną działkę", "LANDAREA", new TriangleFunction(500000D, 470000D), new Range(0D, 0D));
+
+        Quantifier duzoQuant = new Quantifier("dużo", new TriangleFunction(0.75D, 0.26D));
+        Quantifier maloQuant = new Quantifier("mało", new TriangleFunction(0.25D, 0.26D));
+        //Quantifier pojebanyQuant = new Quantifier("chuj mnie to obchodzi ile", new RectangularFunction(0D, 1D));
+
+        Summarizer bedroomsSum = new Summarizer("wchuj sypialni", "BEDRM", new TriangleFunction(50D, 40D), new Range(0D, 0D));
+
+        List<Qualifier> qualifiers = new ArrayList<>();
+        qualifiers.add(bathrooms);
+        qualifiers.add(landarea);
+        List<Quantifier> quantifiers = new ArrayList<>();
+        quantifiers.add(maloQuant);
+        quantifiers.add(duzoQuant);
+        List<Summarizer> summarizers = new ArrayList<>();
+        summarizers.add(bedroomsSum);
+
+        Summary summary = createSummary(dcResMeasurements, quantifiers, qualifiers, summarizers);
+        System.out.println(summary.getContent());
+    }
+
+    private static Summary createSummary(List<?> dcResMeasurements, List<Quantifier> quantifiers, List<Qualifier> qualifiers, List<Summarizer> summarizers) {
+        List<FuzzySet> f = new ArrayList<>();
+        for (Qualifier q : qualifiers) {
+            f.add(FuzzySet.with(dcResMeasurements).from(q));
+        }
+        FuzzySet qualifiersSet = FuzzySet.intersect(f.get(0),f.get(1));
+        FuzzySet summarizersSet = FuzzySet.with(dcResMeasurements).from(summarizers.get(0));
+        FuzzySet wyrazenie = FuzzySet.intersect(qualifiersSet,summarizersSet);
+        double finalSizeNormalized = wyrazenie.size() * 1.0/qualifiersSet.size();
+        assert wyrazenie.size() > 0;
+        double max = 0;
+        String name = "";
+        for(Quantifier q : quantifiers) {
+            if (q.calculate(finalSizeNormalized) > max) {
+                max = q.calculate(wyrazenie.size());
+                name = q.getName();
+            }
+        }
+        Summary s = new Summary();
+        String podsumowanie = name + " budynków mających ";
+        for(Qualifier q : qualifiers) {
+            podsumowanie += q.getName() + ", ";
+        }
+        podsumowanie += "ma " + summarizers.get(0).getName();
+        s.setContent(podsumowanie);
+        return s;
+    }
+
+        /*FuzzySet bathrooms = FuzzySet.with(dcResMeasurements)
+                .from(new Qualifier("dużo łazienek","BATHRM", new TriangleFunction(5D,2D),new Range(3D,4D)));
+
+        FuzzySet landarea = FuzzySet.with(dcResMeasurements)
+                .from(new Qualifier("ogromną działkę", "LANDAREA",new TriangleFunction(500000D,470000D),new Range(0D,0D)));
+        Double aDouble = FuzzySet.sumWithCardinality(fz, 1);
+        System.out.println("sup size: " + fz.support().size());
+        System.out.println(aDouble);
         log.info("Created fuzzySet");
         log.info(String.valueOf(fz.size()));
 
         for (Object entry : fz.entrySet()) {
             System.out.println(String.valueOf(((Map.Entry<DCResMeasurement,Double>)entry).getKey().getNoBathrooms()) + " u= " + String.valueOf(((Map.Entry<DCResMeasurement,Double>)entry).getValue()));
-        }
-
-    }
+        }*/
 
 
 }
