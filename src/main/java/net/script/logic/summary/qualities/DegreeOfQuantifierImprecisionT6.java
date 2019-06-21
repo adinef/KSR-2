5720a26f1ddf5882e67241cbba17a258ab55c715
s207230 <207230@edu.p.lodz.pl>
2019-06-21 17:08:37 +0200

    LAST

--- src/main/java/net/script/logic/summary/qualities/DegreeOfQuantifierImprecisionT6.java
+++ src/main/java/net/script/logic/summary/qualities/DegreeOfQuantifierImprecisionT6.java
@@ -15,10 +15,6 @@ public class DegreeOfQuantifierImprecisionT6  {
 
     public static double calculateDegreeOfQuantifierImprecision(SummarizationState summarizationState, String name) {
         Quantifier quantifier = summarizationState.getQuantfiers().stream().filter(s -> s.getName().trim().equals(name)).collect(Collectors.toList()).get(0);
-        if(quantifier.getFunction().getClass().getName().equals("TriangularFunction")) {
-            return 1.0 - quantifier.getFunction().distance();
-        } else {
-            return 1.0 - quantifier.getFunction().distance()/2;
-        }
+        return 1.0 - quantifier.getFunction().distance();
     }
 }
